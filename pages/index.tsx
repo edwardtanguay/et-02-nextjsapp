fad4eb1f8cc1ea3e488111428eb1145935a0b3d2
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-13 15:51:12 +0100

    welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,26 @@ import styles from '../styles/Index.module.scss';
 import { Nav } from '../components/Nav';
 import { SiteTitle } from '../components/SiteTitle';
 import { DynamicHead } from '../components/DynamicHead';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<section className="siteHeader">
-				<DynamicHead/>
+				<DynamicHead />
 				<SiteTitle />
 				<Nav />
 			</section>
 			<section className="sitePage">
-				<p className={styles.normal}>image goes here</p>
+				<p className={styles.normal}>Welcome to this site.</p>
+				<Image
+				 	className={styles.normal}
+					src="/images/screenWithCode.jpg"
+					alt="screen with code"
+					width={400}
+					height={264}
+					objectFit="contain"
+				/>
 			</section>
 		</>
 	);
